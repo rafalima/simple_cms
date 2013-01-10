36aaa2dfe4014109ad1a78008b2e3db929a8a10a
Rafael Lima <rafalima07@gmail.com>
2013-01-10 19:37:10 -0300

    adding new stuff

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,4 +1,19 @@
 class DemoController < ApplicationController
-  def index
+  def index	
+	@id = params[:id].to_i
+	@page = params[:page].to_i
+	render('index')
   end
+  
+  def hello
+	#redirect_to(:action => 'index')	
+	@array = [1,2,3,4]
+  end
+  
+  def other_hello
+	render(:text => 'something')
+  end
+	
+  
+  
 end
