e3670edcff258433d73806d2c164e3b869622168
Rafael Lima <rafalima07@gmail.com>
2013-04-11 14:58:39 -0300

    adding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  
+  before_filter :confirm_logged_in
+  
+  
+  protected
+  
+  def confirm_logged_in
+    unless session[:user_id]
+      flash[:notice] = "Please log in."
+      redirect_to(:controller => "access", :action => "login")
+      return false #halts the before filter
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 SimplesCms::Application.routes.draw do
 
   root :to => "demo#index"
+  
+  match 'admin', :to => 'access#menu'
+  
+  match ':controller(/:action(/:id))(.:format)'
+  
+  
   # get "demo/index"
 
   # The priority is based upon order of creation:
@@ -58,5 +64,5 @@ SimplesCms::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  match ':controller(/:action(/:id))(.:format)'
+  
 end
