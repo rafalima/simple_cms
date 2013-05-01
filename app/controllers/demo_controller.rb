7b92287f1c601724c50d327b7eee4c5837f6e16b
Rafael Lima <rafalima07@gmail.com>
2013-05-01 20:30:43 -0300

    finishing lynda class

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -28,6 +28,22 @@ class DemoController < ApplicationController
 	def escape_output
 	  
 	end
-  
-  
+
+  def make_error
+    # My guesses for the 3 most common errors:
+    #render(:text => "test" # syntax error, unexpected X, expecting Y
+    #render(:text => @something.upcase) # undefined method on nil
+    #render(:text => "1" + 1) # can't convert type
+  end
+
+  def logging
+    @subjects = Subject.all
+    ActiveSupport::Deprecation.warn("This is a deprecation")
+    logger.debug("This is debug.")
+    logger.info("This is info.")
+    logger.warn("This is warn.")
+    logger.error("This is error.")
+    logger.fatal("This is fatal.")
+    render(:text => 'Logged!')
+  end
 end
--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,6 +1,8 @@
 class PublicController < ApplicationController
   layout 'public'
 
+  before_filter :setup_navigation
+
   def index
     #intro text
   end
@@ -8,5 +10,11 @@ class PublicController < ApplicationController
   def show
     @page = Page.where(:permalink => params[:id], :visible => true).first
     redirect_to(:action => 'index') unless @page
+  end
+
+  private
+
+  def setup_navigation
+    @subjects = Subject.visible.sorted
   end
 end
